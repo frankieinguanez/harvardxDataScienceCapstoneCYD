a911a3efdcf942fdd25faf48c560d4d07167e296
Frankie Inguanez <frankieinguanez@gmail.com>
2022-09-28 15:37:05 +0200

    Updated report.

--- script.r
+++ script.r
@@ -1,3 +1,6 @@
+
+# Dry Beans Dataset from: https://archive.ics.uci.edu/ml/datasets/Dry+Bean+dataset
+
 ##########################################################
 # Global Variables
 ##########################################################
